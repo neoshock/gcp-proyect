26b16351e03464915cd0498d884a23af810a32fa
lalcivar <lalcivar@lbcdevops.com>
2025-05-17 15:09:43 -0500

    update video iframe google drive

--- src/app/components/VideoModal.tsx
+++ src/app/components/VideoModal.tsx
@@ -22,18 +22,17 @@ export const VideoModal = ({ isOpen, onClose }: Props) => {
             <div className="relative bg-white rounded-2xl overflow-hidden shadow-xl w-full max-w-2xl">
                 <button
                     onClick={onClose}
-                    className="absolute top-2 right-2 text-black hover:text-red-600 text-xl font-bold z-10"
+                    className="absolute top-2 right-2 text-white bg-red-600 hover:bg-red-700 text-xl font-bold z-10 px-4 py-2 rounded-full"
                 >
-                    ×
+                    x
                 </button>
-                <video
-                    controls
-                    className="w-full max-h-[80vh] object-contain rounded-b-2xl"
-                >
-                    <source src="/videos/video-tutorial.mp4" type="video/mp4" />
-                    Tu navegador no soporta el video.
-                </video>
-
+                <iframe
+                    src="https://drive.google.com/file/d/1hTTc4ZMxO5cnuVhiA2zWwkzoxfz29fS4/preview"
+                    width="100%"
+                    height="480"
+                    allow="autoplay"
+                    className="rounded-b-2xl w-full max-h-[80vh]"
+                ></iframe>
             </div>
         </div>
     );
